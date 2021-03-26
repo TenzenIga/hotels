09f4ea844e8c2e2567fd9b748e2eb125da9f69a1
TenzenIga <arhan-93@list.ru>
2021-03-26 13:56:07 +0300

    fix: change to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router  } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom'
 import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
